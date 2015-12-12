25ca5a70780a114e80d7fc4bc9424e305dd90e9a
James Fitzpatrick <thefishlive@hotmail.com>
2015-12-12 16:54:05 +0000

    Make door only open when both buttons are pressed

---
+++ Assets/Game Content/Scripts/World/ScriptingController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptingController : MonoBehaviour 
+{
+    public int RequiredSignals;
+    public Interactable Target;
+
+    private int signals = 0;
+
+    public void SendSignal(bool signal)
+    {
+        signals += signal ? 1 : -1;
+        Debug.Log(signals);
+
+        if (signals >= RequiredSignals)
+        {
+            Target.interact();
+        }
+    }
+}
---
+++ Assets/Game Content/Scripts/World/SignalLightController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SignalLightController : Interactable 
+{
+    public ScriptingController Controller;
+
+    public Material OnMaterial;
+    public Material OffMaterial;
+    public GameObject Light;
+
+    public bool State;
+
+    public void SetState(bool state)
+    {
+        Debug.Log(state);
+        State = state;
+        Light.GetComponent<MeshRenderer>().material = State ? OnMaterial : OffMaterial;
+        Controller.SendSignal(State);
+    }
+
+    public void Toggle()
+    {
+        SetState(!State);
+    }
+
+    public override void interact()
+    {
+        Toggle();
+    }
+
+}
