cf1654b061fe666bc9fd70fb5d7998eea77c14b8
James Fitzpatrick <thefishlive@hotmail.com>
2015-12-14 02:54:15 +0000

    Port to webgl

---
+++ Assets/Game Content/Scripts/PlayMaker/ShowWaypoint.cs
@@ -0,0 +1,20 @@
+﻿using HutongGames.PlayMaker;
+
+[ActionCategory(ActionCategory.Logic)]
+[Tooltip("Shows a waypoint")]
+public class ShowWaypoint : FsmStateAction
+{
+    [UIHint(UIHint.Variable)]
+    [Tooltip("")]
+    public UnityEngine.GameObject Waypoint;
+
+    public override void Reset()
+    {
+    }
+
+    public override void OnEnter()
+    {
+        Waypoint.SetActive(true);
+        Finish();
+    }
+}
